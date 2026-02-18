7f60bcaca823d0fd141d21d764dde081b0a2e5b4
Thalisson Rodrigues <thalissonsv22@gmail.com>
2026-02-18 00:41:45 -0300

    Create an autoload to retrieve items.
    Creates an autoload to retrieve items from the database.

--- addons/gd_item_manager/gd_item_manager.gd
+++ addons/gd_item_manager/gd_item_manager.gd
@@ -2,19 +2,21 @@
 extends EditorPlugin
 
 
-const SETTING_ITEM_DATABASE_PATH = "addons/gd_item_manager/general/item_database_path"
-const DEFAULT_DATABASE_PATH = "res://item_database.cfg"
+const AUTOLOAD_NAME: String = "GDItemDatabase"
 
 
 func _enable_plugin() -> void:
-	# Check if the configuration already exists if not, create a default one.
-	if not ProjectSettings.has_setting(SETTING_ITEM_DATABASE_PATH):
-		ProjectSettings.set_setting(SETTING_ITEM_DATABASE_PATH, DEFAULT_DATABASE_PATH)
+	# Create autoload.
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/gd_item_manager/scripts/gd_item_database.gd")
 
-	ProjectSettings.set_initial_value(SETTING_ITEM_DATABASE_PATH, DEFAULT_DATABASE_PATH)
+	# Check if the configuration already exists if not, create a default one.
+	if not ProjectSettings.has_setting(GDItemManagerSettings.SETTING_ITEM_DATABASE_PATH):
+		ProjectSettings.set_setting(GDItemManagerSettings.SETTING_ITEM_DATABASE_PATH, GDItemManagerSettings.DEFAULT_DATABASE_PATH)
+
+	ProjectSettings.set_initial_value(GDItemManagerSettings.SETTING_ITEM_DATABASE_PATH, GDItemManagerSettings.DEFAULT_DATABASE_PATH)
 
 	var property_info = {
-		"name": SETTING_ITEM_DATABASE_PATH,
+		"name": GDItemManagerSettings.SETTING_ITEM_DATABASE_PATH,
 		"type": TYPE_STRING,
 		"hint": PROPERTY_HINT_FILE,
 		"hint_string": "*.cfg"
@@ -26,9 +28,12 @@ func _enable_plugin() -> void:
 
 
 func _disable_plugin() -> void:
+	# Removes autoload.
+	remove_autoload_singleton(AUTOLOAD_NAME)
+
 	# Removes a setting when disabling the plugin.
-	if ProjectSettings.has_setting(SETTING_ITEM_DATABASE_PATH):
-		ProjectSettings.set_setting(SETTING_ITEM_DATABASE_PATH, null)
+	if ProjectSettings.has_setting(GDItemManagerSettings.SETTING_ITEM_DATABASE_PATH):
+		ProjectSettings.set_setting(GDItemManagerSettings.SETTING_ITEM_DATABASE_PATH, null)
 		ProjectSettings.save()
 
 	print("GD Item Manager: disabled.")
---
+++ addons/gd_item_manager/scripts/gd_item_manager_settings.gd
@@ -0,0 +1,5 @@
+class_name GDItemManagerSettings
+
+const SETTING_ITEM_DATABASE_PATH: String = "addons/gd_item_manager/general/item_database_path"
+
+const DEFAULT_DATABASE_PATH: String = "res://item_database.cfg"
